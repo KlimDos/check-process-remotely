ff6bd471d8dec609e7ced0924f8e2e2d86d6aed0
Sasha Alimov <aalimov@axcient.com>
2018-11-22 18:34:46 +0300

    task 2 done

--- check-process-remotely.sh
+++ check-process-remotely.sh
@@ -4,23 +4,40 @@
 SID_FILE="$1"
 #password in case if we are using pass auth instead key-auth
 PASSWORD="$2"
-#command which we would like to execute
-COMMAND_TO_EXECUTE='ps aux |grep [d]hclient | tr -s " " | cut -d" " -f9,16'
+# specify the command to execute
+MODE="$3"
 #adding current dir to PATH to be able to use sshpass tool
 PATH=$PATH:${PWD}
+NEW_LINE='echo -e "\n" >> /etc/motd'
+
 # simple check for proper command usage
-if ! [ -r "${SID_FILE}" -a -n "${PASSWORD}" ]; then
-    echo -e "\nUsage:\n\t$0 <sid_file> <root user pwd>\n" 
+if ! [ -r "${SID_FILE}" -a -n "${PASSWORD}" -a -n "${MODE}" ]; then
+    echo -e "\nUsage:\n\t$0 <sid_file> <root user pwd> <mode option> could be 1/2/3 \n" 
     exit 1
 fi
+
 # check the ability of sshpass to be executed 
 if ! which sshpass &>/dev/null; then
     echo "You should copy sshpass to your current or PATH directory"
     exit 1
 fi
+
+#command which we would like to execute
+if [ ${MODE} -eq 1 ]; then
+    COMMAND_TO_EXECUTE='ps aux |grep [d]hclient | tr -s " " | cut -d" " -f9,16'
+elif [ ${MODE} -eq 2 ]; then
+    COMMAND_TO_EXECUTE=" > /etc/motd && ${NEW_LINE} && cat /proc/version >> /etc/motd && ${NEW_LINE} && cat /proc/cmdline >> /etc/motd && ${NEW_LINE}"
+elif [ ${MODE} -eq 3 ]; then
+    COMMAND_TO_EXECUTE='ps aux |grep [d]hclient | tr -s " " | cut -d" " -f11,18'
+else
+    echo -e "\nUsage:\n\t$0 <sid_file> <root user pwd> <mode option> could be 1/2/3 \n" 
+    exit 1
+fi
+
+
 # the main loop
 while read sid; do 
-    echo "Running script on $sid..."
-    sshpass -p "$PASSWORD" ssh sasha@$sid ${COMMAND_TO_EXECUTE} </dev/null \
+    echo -e "Running \e[100m$COMMAND_TO_EXECUTE\033[m on $sid..."
+    sshpass -p "$PASSWORD" ssh root@$sid ${COMMAND_TO_EXECUTE} </dev/null \
     || echo "Unable execute the request on $sid"
 done < $SID_FILE
