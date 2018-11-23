b2cbe6e6fbb6f7d3c086cf2f75b04039745ceb3b
Sasha Alimov <aalimov@axcient.com>
2018-11-23 16:39:58 +0300

    final commit

--- run-remotely.sh
+++ run-remotely.sh
@@ -4,27 +4,23 @@
 SID_FILE="$1"
 #password in case if we are using pass auth instead key-auth
 PASSWORD="$2"
-#command which we would like to execute
-COMMAND_TO_EXECUTE='ps aux |grep [d]hclient | tr -s " " | cut -d" " -f9,16'
+# specify the command to execute
+MODE="$3"
 #adding current dir to PATH to be able to use sshpass tool
 PATH=$PATH:${PWD}
+NEW_LINE='echo -e "\n" >> /etc/motd'
+
 # simple check for proper command usage
-<<<<<<< HEAD:check-process-remotely.sh
-if ! [ -r "${SID_FILE}" -a -n "${PASSWORD}" ]; then
-    echo -e "\nUsage:\n\t$0 <sid_file> <root user pwd>\n" 
-=======
 if ! [ -r "${SID_FILE}" -a -n "${PASSWORD}" -a -n "${MODE}" ]; then
     echo -e "\nUsage:\n\t$0 <sid_file> <root user pwd> <mode option> could be 1/2 \n" 
->>>>>>> 7d0affe... task 2:run-remotely.sh
     exit 1
 fi
+
 # check the ability of sshpass to be executed 
 if ! which sshpass &>/dev/null; then
     echo "You should copy sshpass to your current or PATH directory"
     exit 1
 fi
-<<<<<<< HEAD:check-process-remotely.sh
-=======
 
 #command which we would like to execute
 if [ ${MODE} -eq 1 ]; then
@@ -37,10 +33,9 @@ else
 fi
 
 
->>>>>>> 7d0affe... task 2:run-remotely.sh
 # the main loop
 while read sid; do 
-    echo "Running script on $sid..."
-    sshpass -p "$PASSWORD" ssh sasha@$sid ${COMMAND_TO_EXECUTE} </dev/null \
+    echo -e "Running \e[100m$COMMAND_TO_EXECUTE\033[m on $sid..."
+    sshpass -p "$PASSWORD" ssh root@$sid ${COMMAND_TO_EXECUTE} </dev/null \
     || echo "Unable execute the request on $sid"
 done < $SID_FILE
