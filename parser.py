7dd5f48029962f610ba8187aad8f8d1a71e06ddd
Sasha Alimov <aalimov@axcient.com>
2018-11-23 16:30:10 +0300

    comment the code

--- parser.py
+++ parser.py
@@ -1,7 +1,11 @@
+
 import json
+# initialize a list
 dataitem = []
+# read payload
 with open('data.json', 'r') as data:
     dataitem = json.load(data)
+# the main loop   
 for num in range(len(dataitem)):
     if "laborum" in dataitem[num]['tags']:
         for friends in dataitem[num]['friends']:
